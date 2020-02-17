be7d2463a33b9049f208db32e0cad18112cd69c2
Vanja Jelic <jelicvanja@yahoo.com>
2020-02-17 10:58:54 +0100

    SINGLE SQL

--- web/modules/custom/evinyl/evinyl_album/src/Controller/AlbumController.php
+++ web/modules/custom/evinyl/evinyl_album/src/Controller/AlbumController.php
@@ -4,9 +4,9 @@
  * Contains \Drupal\evinyl_album\Controller\AlbumController.
  */
 namespace Drupal\evinyl_album\Controller;
-use Drupal\node\Entity\Node;
-use Symfony\Component\HttpFoundation\JsonResponse;
+// use Drupal\node\Entity\Node;
 use Symfony\Component\HttpFoundation\Response;
+use Drupal\file\Entity\File;
 
 class AlbumController {
   public function content($id = 0) {
@@ -18,11 +18,40 @@ class AlbumController {
     // $albums = Node::loadMultiple($albums_nids);
 
     // Load entities by their property values.
-    $entities = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'album']);
+    // $entities = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties(['type' => 'album']);
+    $database = \Drupal::database();
+    $query = $database->query("
+      SELECT `node`.`nid` AS nid,
+      `node`.`uuid` AS uuid,
+      `node_field_data`.`title` AS title,
+      `node__field_image`.`field_image_target_id` AS image_id,
+      `node__field_artist_term`.`field_artist_term_target_id` AS artist_id,
+      `node__field_genre`.`field_genre_target_id` AS genre_id,
+      `taxonomy_term_field_data`.`tid` AS term_id,
+      `taxonomy_term_field_data`.`name` AS term_name
+      FROM `node`
+      INNER JOIN `node_field_data` ON `node`.`nid` = `node_field_data`.`nid`
+      INNER JOIN `node__field_image` ON `node`.`nid` = `node__field_image`.`entity_id`
+      INNER JOIN `node__field_artist_term` ON `node`.`nid` = `node__field_artist_term`.`entity_id`
+      INNER JOIN `node__field_genre` ON `node`.`nid` = `node__field_genre`.`entity_id`
+      INNER JOIN `taxonomy_term_field_data` ON `node__field_genre`.`entity_id` = `taxonomy_term_field_data`.`tid`
+      WHERE node_field_data.status = '1' AND node.type = 'album'
+    ");
+      // ORDER BY "
+
+    $entities = $query->fetchAll();
+    $albums = $this->buildNodesArray($entities);
+
+    // $artists_query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('artists');
+    // $genres_query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('genre');
+    // $artists = $this->buildTermsArray($artists_query);
+    // $genres = $this->buildTermsArray($genres_query);
 
     $return_object = [
-      'count' => count($entities),
-      'data' => $entities
+      'count' => count($albums),
+      'albums' => $albums,
+      // 'artists' => $artists,
+      // 'genres' => $genres,
     ];
 
     $cache_options = [
@@ -35,12 +64,57 @@ class AlbumController {
     $response = new Response();
     $response->setContent($json_data);
     $response->headers->set('Content-Type', 'application/json');
-    $response->setCache($cache_options);
+    // $response->setCache($cache_options);
     return $response;
-
-    // return array(
-    //   '#type' => 'markup',
-    //   '#markup' => t('Not neccessary any more. Use core JSON:API'),
-    // );
   }
+
+  public function buildTermsArray($terms) {
+    $output = [];
+    // var_dump($terms);
+    foreach($terms as $term) {
+      array_push($output, array(
+        'tid' => $term->tid,
+        'name' => $term->name
+      ));
+    }
+    return $output;
+  }
+
+  // `node`.`uuid` AS uuid,
+  // `node_field_data`.`title` AS title,
+  // `node__field_image`.`field_image_target_id` AS image_id,
+  // `node__field_artist_term`.`field_artist_term_target_id` AS artist_id,
+  // `node__field_genre`.`field_genre_target_id` AS genre_id,
+  // `taxonomy_term_field_data`.`name` AS term_name
+  public function buildNodesArray($nodes) {
+    $output = [];
+    $style = \Drupal::entityTypeManager()->getStorage('image_style')->load('thumbnail');
+    foreach($nodes as $node) {
+      $file = File::load($node->image_id);
+      $thumb = $style->buildUrl($file->uri->value);
+      $image_url = $file->uri;
+      $cover = ['thumb' => $thumb, 'image' => $file->uri];
+      $artist_name = '';
+      $genre_name = '';
+      if ($node->term_id == $node->artist_id) {
+        $artist_name = $node->term_name;
+      }
+      if ($node->term_id == $node->genre_id) {
+        $genre_name = $node->term_name;
+      }
+
+      array_push($output, array(
+        'name' => $node->title,
+        'nid' => $node->nid,
+        'uuid' => $node->uuid,
+        'tid' => $node->term_id,
+        'artist' => [id => $node->artist_id, name => $artist_name],
+        'genre' => [id => $node->genre_id, name => $genre_name],
+        'cover' => $cover,
+        'path' => \Drupal::service('path.alias_manager')->getAliasByPath('/node/'.$node->nid),
+      ));
+    }
+    return $output;
+  }
+
 }
