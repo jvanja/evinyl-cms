9bdbccea895f9e02a3989b80fd944b39826c0955
Vanja Jelic <jelicvanja@yahoo.com>
2020-04-29 16:06:46 +0200

    converted h to s

--- web/modules/custom/evinyl/evinyl_discogs/src/Controller/EvinylDiscogsController.php
+++ web/modules/custom/evinyl/evinyl_discogs/src/Controller/EvinylDiscogsController.php
@@ -193,7 +193,7 @@ class EvinylDiscogsController extends ControllerBase {
       }
       $song_paragraph = Paragraph::create([
         'type'                => $paragraphName,
-        'field_song_duration' => $track->duration,
+        'field_song_duration' => $this->hhmmss_to_seconds($track->duration),
         'field_song_name'     => $track->title,
         'field_song_credits'  => $creditsString
       ]);
@@ -202,4 +202,10 @@ class EvinylDiscogsController extends ControllerBase {
     return $paragraphs;
   }
 
+  private function hhmmss_to_seconds($str_time = '0:0') {
+    $str_time = preg_replace("/^([\d]{1,2})\:([\d]{2})$/", "00:$1:$2", $str_time);
+    sscanf($str_time, "%d:%d:%d", $hours, $minutes, $seconds);
+    $time_seconds = $hours * 3600 + $minutes * 60 + $seconds;
+    return $time_seconds;
+  }
 }
