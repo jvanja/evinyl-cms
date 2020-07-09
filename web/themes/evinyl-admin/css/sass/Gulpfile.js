076b7c5921c3ce99245cef7b9229b74c2aa22c32
Vanja Jelic <jelicvanja@yahoo.com>
2020-07-09 11:46:05 +0200

    merge attempt

---
+++ web/themes/evinyl-admin/css/sass/Gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./evinyl_admin.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('../'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('sass:watch'));
