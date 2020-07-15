81eed4d34a7b1f8a631596836e04110b07c75241
Vanja Jelic <jelicvanja@yahoo.com>
2020-07-15 17:11:42 +0200

    added all kinds of node album add edit tweaks

---
+++ web/themes/evinyl-admin/js/evinyl_admin_album_alter.js
@@ -0,0 +1,13 @@
+(function ($, Drupal) {
+ Drupal.behaviors.evinyl_album = {
+    attach: function (context, settings) {
+
+      if(settings.path.currentPath.includes('/edit')) {
+
+        $('#tracks details').attr('open', 'true')
+      }
+    }
+  };
+
+})(jQuery, Drupal);
+
