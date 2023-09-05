c6e3a1ba2bf829a6f95a12064b7064aea033ffb2
Vanja Jelic <1101520+jvanja@users.noreply.github.com>
2023-09-05 12:49:47 +0200

    added button and loading

--- web/modules/custom/evinyl/evinyl_lyrics/js/evinyl_lyrics.js
+++ web/modules/custom/evinyl/evinyl_lyrics/js/evinyl_lyrics.js
@@ -9,14 +9,25 @@
     attach: function (context) {
       if (context.classList && context.classList.contains('page-node-type-album')) {
         once('getLyricsBehariour', 'html').forEach(function (element) {
-          processAlbumHtml(element)
+          const isAdmin = context.classList.contains('role_administrator')
+          if (isAdmin) {
+            processAlbumHtml(element)
+          }
         })
       }
       function processAlbumHtml(elem) {
         // - TODO:  <09/04/2023, vanja> -
         // Append a real button somewhere on the page
-        const addLyricsButton = document.querySelector('#field-a-side-songs-values h4')
-        addLyricsButton.addEventListener('click', () => {
+        let button = document.createElement('button')
+        button.setAttribute('id', 'add-lyrics')
+        button.setAttribute('class', 'button button--primary')
+        button.setAttribute('style', 'margin-bottom: 20px;')
+        button.innerHTML = 'Add lyrics from MusicMatch'
+        elem.querySelector('#tracks #field-a-side-songs-add-more-wrapper').prepend(button)
+
+        const addLyricsButton = elem.querySelector('#add-lyrics')
+        addLyricsButton.addEventListener('click', (e) => {
+          e.preventDefault()
           getLyricsFromMusicMatch(elem)
         })
       }
@@ -38,32 +49,45 @@
 
         // search the musicmatch API for the songs
         if (artistName.length > 1) {
-          songsNamesFields.forEach(async (field) => {
-            const songIdArr = field.classList.value.split(' ').filter((cl) => cl.indexOf('paragraph-id--') == 0)
-            const songId = songIdArr[0].replace('paragraph-id--', '')
+          songsNamesFields.forEach(async (song_paragraph) => {
+            // add the AJAX laoder animation
+            addStatusToTrackElement(song_paragraph.querySelector('.layout__region--second'), 'loading')
 
-            const songName = field.querySelector('.field--name-field-song-name').innerText
+            const songIdArr = song_paragraph.classList.value
+              .split(' ')
+              .filter((cl) => cl.indexOf('paragraph-id--') == 0)
+            const songId = songIdArr[0].replace('paragraph-id--', '')
+            const songName = song_paragraph.querySelector('.field--name-field-song-name').innerText
             const queryParams = `q_artist=${encodeURIComponent(artistName)}&q_track=${encodeURIComponent(songName)}`
             const apiEndPoint = apiEndPointBase + queryParams + apiKey
-            const response = await fetch(apiEndPoint)
-            const lyrics = await response.json()
+
+            // call the musicmatch API
+            try {
+              const response = await fetch(apiEndPoint)
+              const lyrics = await response.json()
+              if (lyrics.message.header.status_code === 200) {
+                // update the CMS with the new lyrics
+                updateTrackWithLyrics(songId, lyrics.message.body.lyrics.lyrics_body, song_paragraph)
+
+                // add the musicmatch tracker script
+                const trackingScript = lyrics.message.body.lyrics.script_tracking_url
+                addMusicMatchTrackerScript(trackingScript)
+
+                // TODO: add lyrics copyright message
+                const copyrightMessage = lyrics.message.body.lyrics.lyrics_copyright
+              } else {
+                addStatusToTrackElement(song_paragraph.querySelector('.layout__region--second'), 'error')
+              }
+            } catch (error) {
+              addStatusToTrackElement(song_paragraph.querySelector('.layout__region--second'), 'error')
+            }
 
             // update each tracks with new lyrics
-            if (lyrics.message.header.status_code === 200) {
-              updateTrackWithLyrics(songId, lyrics.message.body.lyrics.lyrics_body)
-
-              // add the musicmatch tracker script
-              const trackingScript = lyrics.message.body.lyrics.script_tracking_url
-              addMusicMatchTrackerScript(trackingScript)
-
-              // TODO: add lyrics copyright message
-              const copyrightMessage = lyrics.message.body.lyrics.lyrics_copyright
-            }
           })
         }
       }
 
-      async function updateTrackWithLyrics(id, lyricsResponse) {
+      async function updateTrackWithLyrics(id, lyricsResponse, song_paragraph) {
         const lyrics = '<p>' + lyricsResponse.replaceAll('\n', '<br/>') + '</p>'
 
         const lyricsApi = window.drupalSettings.path.baseUrl + 'admin/api/lyrics'
@@ -78,11 +102,33 @@
           }),
         })
         const responseJson = await updateTracksResponse.json()
+        addStatusToTrackElement(song_paragraph.querySelector('.layout__region--second'), 'success')
         console.log(responseJson)
       }
 
       function addMusicMatchTrackerScript(script) {
         console.log('adding tracking:', script)
+      }
+
+      function addStatusToTrackElement(element, status) {
+        if (document.querySelector('#tracks .lyrics-loader-status'))
+          document.querySelector('#tracks .lyrics-loader-status').remove()
+        const statusElement = document.createElement('div')
+        statusElement.setAttribute('class', 'lyrics-loader-status')
+        element.appendChild(statusElement)
+        switch (status) {
+          case 'loading':
+            statusElement.classList.add('loading')
+            break
+          case 'success':
+            statusElement.classList.add('success')
+            break
+          case 'error':
+            statusElement.classList.add('error')
+            break
+          default:
+            break
+        }
       }
     },
   }
