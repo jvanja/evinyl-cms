60046216b2c656f7ca3c19132382c99c3b88e794
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-10-29 11:00:40 +0100

    cleaned up album controller

--- web/modules/custom/evinyl/evinyl_album/src/Controller/AlbumController.php
+++ web/modules/custom/evinyl/evinyl_album/src/Controller/AlbumController.php
@@ -7,28 +7,26 @@
 
 namespace Drupal\evinyl_album\Controller;
 
-// use Drupal\node\Entity\Node;
 use Drupal\file\Entity\File;
-use Drupal\paragraphs\Entity\Paragraph;
 use Symfony\Component\HttpFoundation\Response;
 
 class AlbumController {
 
+  const DEFAULT_IMAGE_FID = 137;
+  // const FEATURED_GENRE_ID = 62;
+
   public function buildNodesArray($nodes) {
-    // global $base_url;
-    // $root_path = str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
 
     $output = [];
-    $FEATURED_GENRE_ID = 62;
-    $thumb_style = \Drupal::entityTypeManager()->getStorage('image_style')->load('thumbnail');
+    /** @var \Drupal\image\Entity\ImageStyle $thumb_style */
+    $thumb_style =  \Drupal::entityTypeManager()->getStorage('image_style')->load('thumbnail');
+    /** @var \Drupal\image\Entity\ImageStyle $medium_style */
     $medium_style = \Drupal::entityTypeManager()->getStorage('image_style')->load('medium');
-    $artists_ids = [];
-    $genres_ids = [];
     $likes_ids = [];
 
     foreach ($nodes as $node) {
       // set default image (fid=137) if one is not found.
-      $node->image_id = $node->image_id === NULL ? 137 : $node->image_id;
+      $node->image_id = $node->image_id === NULL ? self::DEFAULT_IMAGE_FID : $node->image_id;
       $file = File::load($node->image_id);
       $thumb = $thumb_style->buildUrl($file->uri->value);
       $medium = $medium_style->buildUrl($file->uri->value);
@@ -39,7 +37,6 @@ class AlbumController {
       if ($key !== FALSE) {
         $output[$key]['featured'] = $node->featured;
 
-        // $output[$key]['featured'] = in_array($FEATURED_GENRE_ID, $output[$key]['genres_ids']) ? '1' : '0';
         if ($node->genre_id && !\in_array($node->genre_id, $output[$key]['genres_ids'], TRUE)) {
           $output[$key]['genres_ids'][] = (int) $node->genre_id;
         }
@@ -51,13 +48,12 @@ class AlbumController {
         if ($node->likes && !\in_array($node->likes, $output[$key]['likes_ids'], TRUE)) {
           $output[$key]['likes_ids'][] = $node->likes;
         }
-      }
-      else {
+      } else {
         $output[] = [
           'name' => $node->title,
           'id' => $node->nid,
           'uuid' => $node->uuid,
-          // 'featured' => in_array($FEATURED_GENRE_ID, [(int)$node->genre_id]) ? '1' : '0',
+          // 'featured' => in_array(self::FEATURED_GENRE_ID, [(int)$node->genre_id]) ? '1' : '0',
           'featured' => $node->featured,
           'likes_ids' => array_unique($likes_ids),
           'artists_ids' => [(int) $node->artist_id],
@@ -130,21 +126,11 @@ class AlbumController {
     $artists = $this->buildTermsArray($terms, 'artists');
     $genres = $this->buildTermsArray($terms, 'genre');
 
-    // $artists_query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('artists');
-    // $genres_query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree('genre');
-    // $artists = $this->buildTermsArray($artists_query);
-    // $genres = $this->buildTermsArray($genres_query);
-
     $return_object = [
       'count' => \count($albums),
       'albums' => $albums,
       'artists' => $artists,
       'genres' => $genres,
-    ];
-
-    $cache_options = [
-      'public' => TRUE,
-      'max_age' => '31536000',
     ];
 
     $serializer = \Drupal::service('serializer');
@@ -153,28 +139,26 @@ class AlbumController {
     $response->setContent($json_data);
     $response->headers->set('Content-Type', 'application/json');
 
-    // $response->setCache($cache_options);
     return $response;
   }
 
-  public function update() {
-    $export = '';
-    $database = \Drupal::database();
-    $paras = $database->query('SELECT entity_id, field_image_target_id FROM {paragraph__field_image}');
-    $paras_res = $paras->fetchAll();
-    $targets = implode(',', array_column($paras_res, 'field_image_target_id'));
-
-    foreach ($paras_res as $para) {
-      $paragraph_id = $para->entity_id;
-      $paragraph = Paragraph::load($paragraph_id);
-      $paragraph->set('field_gallery_image', $para->field_image_target_id);
-      $paragraph->save();
-    }
-
-    return [
-      '#type' => 'markup',
-      '#markup' => t($export . 'All done'),
-    ];
-  }
+  // public function update() {
+  //   $database = \Drupal::database();
+  //   $paras = $database->query('SELECT entity_id, field_image_target_id FROM {paragraph__field_image}');
+  //   $paras_res = $paras->fetchAll();
+  //   // $targets = implode(',', array_column($paras_res, 'field_image_target_id'));
+  //
+  //   foreach ($paras_res as $para) {
+  //     $paragraph_id = $para->entity_id;
+  //     $paragraph = Paragraph::load($paragraph_id);
+  //     $paragraph->set('field_gallery_image', $para->field_image_target_id);
+  //     $paragraph->save();
+  //   }
+  //
+  //   return [
+  //     '#type' => 'markup',
+  //     '#markup' => 'All done',
+  //   ];
+  // }
 
 }
