fcc0c6b4c6a9083b6a47d47006b78f6d99569f43
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2024-11-18 10:18:59 +0100

    added nodejs exists CHECKS

--- web/modules/custom/evinyl/evinyl_album/src/Controller/GetAuthController.php
+++ web/modules/custom/evinyl/evinyl_album/src/Controller/GetAuthController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Contains \Drupal\evinyl_album\Controller\GetJsonController.
@@ -6,34 +7,19 @@
 namespace Drupal\evinyl_album\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Session;
 
 class GetAuthController {
 
   public function content() {
-    /**
-     * Get an auth token for the current user.
-     */
-    // function nodejs_auth_get_token(SessionInterface $session) {
-    //   $nodejs_auth_get_token_callback = \Drupal::config('nodejs.config')->get('auth_get_token_callback');
-    //   if (!function_exists($nodejs_auth_get_token_callback)) {
-    //     throw new Exception("Cannot proceed without a valid nodejs_auth_get_token callback - looked for '$nodejs_auth_get_token_callback'.");
-    //   }
-    //   return $nodejs_auth_get_token_callback($session);
-    // }
-    //
-    //
     $response = new JsonResponse();
     $session = \Drupal::request()->getSession();
 
-    // $response->setData(nodejs_auth_get_token($_SESSION));
-    $response->setData(['authToken' => nodejs_auth_get_token($session)]);
-
+    if (function_exists('nodejs_auth_get_token')) {
+      /** @disregard The function is exposed via the nodejs module */
+      $response->setData(['authToken' => nodejs_auth_get_token($session)]);
+    } else {
+      $response->setData(['authToken' => '']);
+    }
     return $response;
-    // return array(
-    //   '#type' => 'markup',
-    //   '#markup' => t('New albums.json generated'),
-    // );
   }
-
 }
